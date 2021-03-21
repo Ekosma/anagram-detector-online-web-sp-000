a682c1d457293e117055086dfbcaa6e0c51c6338
Evelyn Kosma <ezranaja@gmail.com>
2021-03-21 18:29:55 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  def initalize(word)
+    @word = word
+  end
+  
+  def self.match(array)
+    matches = []
+    new_word = @word.split("").sort 
+    array.each do |word| 
+      new_array = word.split("").sort  
+      if new_word == new_array 
+        new_array >> matches
+    end
+    matches
+  end
+end
